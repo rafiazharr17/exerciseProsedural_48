e22d6d1c34527933844ccdfe844dfc710c059caf
rafiazharr17 <rafipilo17@gmail.com>
2023-03-29 14:48:17 +0700

    Add project files.

---
+++ exerciseProsedural_048/exerciseProsedural_048.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+int Kandidat = 3;
+string nama[3];
+double nilaiM[3];
+double nilaiBing[3];
+
+void inputan() {
+	for (int i = 0; i < Kandidat; i++) {
+		cout << "Data Kandidat Ke- " << i + 1 << endl;
+		cout << "Nama Kandidat : ";
+		cin >> nama[i];
+		cout << "Masukkan Nilai Matematika : ";
+		cin >> nilaiM[i];
+		cout << "Masukkan Nilai Bahasa Inggris : ";
+		cin >> nilaiBing[i];
+		cin.ignore();
+
+		cout << endl;
+	}
+}
+
+bool diterima(int index) {
+	return(nilaiM[index] > 80 or (nilaiM[index] + nilaiBing[index]) / 2 >= 70);
+}
+
+void tampilkanData() {
+	cout << "Hasil Data Kandidat : " << endl;
+	cout << "Nama" << "\t\t" << "Status" << endl;
+	cout << endl;
+	for (int i = 0; i < Kandidat; i++) {
+		cout << nama[i] << "\t\t";
+		if (diterima(i)) {
+			cout << "Diterima";
+		}
+		else {
+			cout << "Ditolak";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void hitungjumlahditerima() {
+	int yangditerima = 0;
+	int yangditolak = 0;
+	for (int i = 0; i < Kandidat; i++) {
+		if (diterima(i)) {
+			yangditerima++;
+		}
+		else {
+			yangditolak++;
+		}
+	}
+	cout << "Jumlah Kandidat yang Diterima	: " << yangditerima << endl;
+	cout << "Jumlah Kandidat yang Ditolak	: " << yangditolak << endl;
+}
+
+int main() {
+	inputan();
+	tampilkanData();
+	hitungjumlahditerima();
+
+	return 0;
+}
